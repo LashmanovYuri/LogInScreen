310419ff4955cedeacbcc04601de572fd1fd780e
LashmanovYuri <39579400+LashmanovYuri@users.noreply.github.com>
2021-06-25 23:41:22 +0400

    1st version

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function() {
+	$(".button").click(function() {
+		$(".form-signin").toggleClass("form-signin-left");
+    $(".form-signup").toggleClass("form-signup-left");
+    $(".frame").toggleClass("frame-long");
+    $(".signup-inactive").toggleClass("signup-active");
+    $(".signin-active").toggleClass("signin-inactive");
+    $(".forgot").toggleClass("forgot-left");   
+    $(this).removeClass("idle").addClass("active");
+	});
+});
+
+$(function() {
+	$(".button-signup").click(function() {
+  $(".navigation").toggleClass("navigation-up");
+  $(".form-signup-left").toggleClass("form-signup-down");
+  $(".success").toggleClass("success-left"); 
+  $(".frame").toggleClass("frame-short");
+	});
+});
+
+$(function() {
+	$(".button-signin").click(function() {
+  $(".button-animate").toggleClass("button-animate-grow");
+  $(".welcome").toggleClass("welcome-left");
+  $(".cover-photo").toggleClass("cover-photo-down");
+  $(".frame").toggleClass("frame-short");
+  $(".profile-photo").toggleClass("profile-photo-down");
+  $(".button-goback").toggleClass("button-goback-up");
+  $(".forgot").toggleClass("forgot-fade");
+	});
+});
